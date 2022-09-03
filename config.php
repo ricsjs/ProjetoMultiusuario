caa0b9033abbcb2606b2624761688cd1b74ad9ea
Ricardo <ricfilho0007@gmail.com>
2022-09-03 15:40:29 -0300

    Arquivo de configuração do banco de dados

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$servidor = "localhost";
+//usuario do db
+$usuario = "root";
+//senha do db
+$senha = "";
+//nome do db
+$dbname = "multiusuario";
+
+//passando paramentros para conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+?>
